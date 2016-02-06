657a10df0562eac0ccea30784ef4e92bd859c6ad
Sheriff Mogaji <shakmog@gmail.com>
2016-02-06 11:43:26 +0100

    page banners, page contents, footer partial and other styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,27 +30,42 @@ $(function() {
 	// Backstretch
 	switch (document.location.href) {
 		case "http://localhost:3003/":
-			$('#header-featured').backstretch(["/assets/banner2.jpg", 
-				"/assets/banner1.jpg", "/assets/banner.jpg",
-				"/assets/banner3.jpg"]);
+			$('#header-featured').backstretch([
+				"/assets/banner2.jpg", 
+				"/assets/banner1.jpg", 
+				"/assets/banner.jpg",
+				"/assets/banner3.jpg",
+				"/assets/banner4.jpg"
+			]);
 			break;
 		case "http://localhost:3003/aboutus":
-			$('#header-featured').backstretch("/assets/aboutus.jpg");
+			$('#header-featured').backstretch(
+				"/assets/aboutus.jpg"
+			);
 			break;
 		case "http://localhost:3003/careers":
-			$('#header-featured').backstretch("/assets/careers.jpg");
+			$('#header-featured').backstretch(
+				"/assets/careers.jpg"
+			);
 			break;
 		case "http://localhost:3003/whatwedo":
-			$('#header-featured').backstretch("/assets/whatwedo.jpg");
+			$('#header-featured').backstretch(
+				"/assets/whatwedo.jpg"
+			);
+			break;
+		case "http://localhost:3003/contact-us":
+			$('#header-featured').backstretch(
+				"/assets/contact.jpg"
+			);
 			break;
 	}
 	
 
 	// Menu Toggle
 
-	$('#menu ul li a').click(function(){
-		$(this).parent().addClass('current_page_item').siblings().removeClass('current_page_item');
-	});
+	// $('#menu ul li a').click(function(){
+	// 	$(this).parent().addClass('current_page_item').siblings().removeClass('current_page_item');
+	// });
 
 });
 
